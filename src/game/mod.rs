8b4a0827061abac5b6a7f1f47d6de25796041f4f
Spheniscine <spheniscine@gmail.com>
2026-06-02 20:04:31 +0800

    add some commons

---
+++ src/game/mod.rs
@@ -0,0 +1,11 @@
+mod card;
+pub use card::*;
+
+// mod skin;
+// pub use skin::*;
+
+// mod game;
+// pub use game::*;
+
+// mod settings;
+// pub use settings::*;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use dioxus::prelude::*;
 
+mod game;
+
 const FAVICON: Asset = asset!("/assets/favicon.ico");
+
+// altered version of KaTeX_Main to include filled "red" suits
+const KATEX_SUITS: Asset = asset!("/assets/KaTeX_Suits.woff2");
+
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
@@ -13,6 +19,16 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Style {
+            r#"
+            @font-face {{
+                font-family: KaTeX_Main;
+                font-style: normal;
+                font-weight: 700;
+                src: url({KATEX_SUITS}) format("woff2");
+            }}
+            "#,
+        }
         Hero {}
 
     }
